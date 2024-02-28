7255d93881144921dde5455936ecee0e44b2478d
joshdotjs <joshdotjs@gmail.com>
2024-02-27 23:44:15 -0600

    began generalizing matrix

--- react/900-app/src/App.jsx
+++ react/900-app/src/App.jsx
@@ -12,7 +12,7 @@ export default function App() {
 
   const [state, setState] = useState({
     'nutrient': [[ 0, 0 ], [ 0, 0 ]],
-    'serving':  [[ 0, 0 ], [ 0, 0 ]],
+    'serving':  [[ 0, 0, 0 ], [ 0, 0, 0 ]],
     'result':   [[ 0, 0 ]],
   });
 
@@ -22,7 +22,7 @@ export default function App() {
     let new_result = [0, 0];
     for (let food = 0; food < 2; food++) {
       const total_servings = new_state.serving[food].reduce((a, b) => a + b, 0);
-      for (let idx = 0; idx < 2; idx++) {
+      for (let idx = 0; idx < state.serving[food].length; idx++) {
         new_result[idx] += new_state.nutrient[food][idx] * total_servings;
       }
     }
@@ -58,20 +58,22 @@ export default function App() {
 
   return (
     <div style={{ height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-      <main style={{ 
-        display: 'grid', 
-        gap: '2rem',
-        gridTemplateRows: 'repeat(5, 1fr)',
-        gridTemplateColumns: 'repeat(4, 1fr)',
-        border: 'solid black 1px',
-        borderRadius: '0.25rem',
-        padding: '1rem',
-        width: 'fit-content',
-      }}>
+      <main
+        className="card card-1"
+        style={{ 
+          display: 'grid', 
+          gap: '2rem',
+          gridTemplateRows: 'repeat(9, 1fr)',
+          gridTemplateColumns: 'repeat(4, 1fr)',
+          border: 'solid black 1px',
+          borderRadius: '0.25rem',
+          padding: '1rem',
+          width: 'fit-content',
+        }}
+      >
         <div style={{ gridRow: '1 / 2', gridColumn: '1 / 2' }}>
           <Cell>Calories: </Cell>
         </div>
-
         <div style={{ gridRow: '2 / 3', gridColumn: '1 / 2' }}>
           <Cell>Protien: </Cell>
         </div>
@@ -79,9 +81,11 @@ export default function App() {
         <div style={{ gridRow: '4 / 5', gridColumn: '1 / 2' }}>
           <Cell>Meal 1: </Cell>
         </div>
-
         <div style={{ gridRow: '5 / 6', gridColumn: '1 / 2' }}>
           <Cell>Meal 2: </Cell>
+        </div>
+        <div style={{ gridRow: '6 / 7', gridColumn: '1 / 2' }}>
+          <Cell>Meal 3: </Cell>
         </div>
 
 
@@ -97,6 +101,9 @@ export default function App() {
         <div style={{ gridRow: '5 / 6', gridColumn: '2 / 3' }}>
           <Cell {...{ state, updateState }} type='serving' food={0} idx={1} />
         </div>
+        <div style={{ gridRow: '6 / 7', gridColumn: '2 / 3' }}>
+          <Cell {...{ state, updateState }} type='serving' food={0} idx={2} />
+        </div>
 
         <div style={{ gridRow: '1 / 2', gridColumn: '3 / 4' }}>
           <Cell {...{ state, updateState }} type='nutrient' food={1} idx={0} />
@@ -110,15 +117,19 @@ export default function App() {
         <div style={{ gridRow: '5 / 6', gridColumn: '3 / 4' }}>
           <Cell {...{ state, updateState }} type='serving' food={1} idx={1} />
         </div>
+        <div style={{ gridRow: '6 / 7', gridColumn: '3 / 4' }}>
+          <Cell {...{ state, updateState }} type='serving' food={1} idx={2} />
+        </div>
 
-        <div style={{ gridRow: '4 / 5', gridColumn: '4 / 5' }}>
+        <div style={{ gridRow: '8 / 9', gridColumn: '4 / 5' }}>
           <Cell {...{ state, updateState }} type='result' food={0} idx={0} />
         </div>
-        <div style={{ gridRow: '5 / 6', gridColumn: '4 / 5' }}>
+        <div style={{ gridRow: '9 / 10', gridColumn: '4 / 5' }}>
           <Cell {...{ state, updateState }} type='result' food={0} idx={1} />
         </div>
 
         <hr style={{ gridRow: '3 / 4', gridColumn: '1 / -1', borderColor: 'black', borderWidth: '1px', background: 'black', borderRadius: '0.25rem', width: '100%'}}/>
+        <hr style={{ gridRow: '7 / 8', gridColumn: '1 / -1', borderColor: 'black', borderWidth: '1px', background: 'black', borderRadius: '0.25rem', width: '100%'}}/>
 
       </main>
     </div>
