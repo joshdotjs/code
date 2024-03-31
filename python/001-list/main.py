b7592744cab66f8cc53ae70a5f19f28a5fc12b15
joshdotjs <joshdotjs@gmail.com>
2024-03-30 20:40:56 -0500

    python arrays and tuples

---
+++ python/001-list/main.py
@@ -0,0 +1,10 @@
+empty_list = []
+arr_1 = [1, 2, 3, 4, 5]
+arr_2 = ['a', 'b', 'c', 'd', 'e']
+print(empty_list)
+print(arr_1)
+print(arr_2)
+print(arr_2[0])
+
+for x in arr_2:
+    print(x)
---
+++ python/002-tuple/main.py
@@ -0,0 +1,13 @@
+empty_tuple = ()
+tuple_1 = 'a',
+tuple_2 = ('a')
+tuple_3 = ('a', 'b', 'c', 'd', 'e')
+print(empty_tuple)
+print(tuple_1)
+print(tuple_2)
+print(tuple_3)
+print(tuple_3[0])
+
+
+for x in tuple_3:
+    print(x)
